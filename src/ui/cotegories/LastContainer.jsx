0192ceaa59d3ceb170428147184885a73faf9bb7
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-07-26 15:34:50 +0500

    Update LastContainer.jsx

--- src/ui/cotegories/LastContainer.jsx
+++ src/ui/cotegories/LastContainer.jsx
@@ -6,7 +6,7 @@ const LastContainer = () => {
     return (
         <div className={styles.LastContainer}>
             <div className={styles.BigContainer}>
-                <img src="https://olmashop.uz/static/new/auto.png" alt="image"/>
+                <img src="https://www.goldenpages.uz/timur/1400909-16/avtozapchasti_rubrics.jpg" alt="image"/>
                 <Link to="/product_list?categories=AvtomobilQismlari" className={styles.info}>
                     <p>Avtomobillar mahsuloti </p>
                     <h4>Avtomobillar uchun ehtiyot qismlar</h4>
@@ -15,14 +15,14 @@ const LastContainer = () => {
             <div className={styles.GridContainers}>
                 <div className={styles.Row}>
                     <Link to="/product_list?categories=Go'zallik va Parvarish" className={styles.GridChilder}>
-                        <img src='https://olmashop.uz/static/new/Beauty.png' alt="Rasm"/>
+                        <img src='https://www.silady.ru/upload-files/category-img/lico.jpg' alt="Rasm"/>
                         <div className={styles.infoClick}>
                             <p>Go'zallik va Parvarish </p>
                             <h4>Kosmetika mahsulotlar</h4>
                         </div>
                     </Link>
                     <Link to="/product_list?categories=Elektronika" className={styles.GridChilder}>
-                        <img alt="elektronika" src="https://olmashop.uz/static/new/electronics.png"/>
+                        <img alt="elektronika" src="https://avatars.mds.yandex.net/get-altay/10637174/2a0000018a3712ce173f87b643e8265e8778/L_height"/>
                         <div className={styles.replaceToPage}>
                             <p>Elektronika</p>
                             <h4>Elektron mahsulotlar</h4>
