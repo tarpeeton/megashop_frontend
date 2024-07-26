fb92db28bd79c97fbe73140a6402606d3f0fed35
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-07-26 14:45:32 +0500

    Update parallax.jsx

--- src/ui/parallax.jsx
+++ src/ui/parallax.jsx
@@ -8,13 +8,13 @@ const Parallax = () => {
             <div className={style.Parralax}
                  style={{width: '100' , height: '100vh' ,
                      backgroundAttachment:"fixed" ,
-                     backgroundImage: `url('https://olmashop.uz/static/assets/document/images/ads-1.png')`,
+                     backgroundImage: `url('https://wallpapercave.com/wp/wp4810879.jpg')`,
                      backgroundSize:'cover' ,
                      backgroundRepeat:'no-repeat',
                  }}>
             </div>
             <div className={style.Parralax}  style={{width: '100' , height: '50vh' ,
-                backgroundImage: `url('https://olmashop.uz/static/assets/document/images/brands/Macbook.png')`,
+                backgroundImage: `url('https://cdn.shopify.com/s/files/1/0761/7970/2080/files/1200x800_0df1150f-f496-4796-a830-d34a862c4696_1200x.jpg?v=1715941961')`,
                 backgroundPosition:'center center' ,
                 backgroundRepeat:'no-repeat',
                 padding: "40px"
