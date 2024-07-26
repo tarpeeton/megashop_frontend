c15ff84f5be798aab826c3c2a232d9733e5b96bf
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-07-26 15:25:51 +0500

    Update parallax.jsx

--- src/ui/parallax.jsx
+++ src/ui/parallax.jsx
@@ -13,12 +13,6 @@ const Parallax = () => {
                      backgroundRepeat:'no-repeat',
                  }}>
             </div>
-            <div className={style.Parralax}  style={{width: '100' , height: '50vh' ,
-                backgroundImage: `url('https://www.cgdirector.com/wp-content/uploads/media/2023/02/Is-MSI-a-Good-Laptop-Brand-Twitter-1200x675.jpg')`,
-                backgroundPosition:'center center' ,
-                backgroundRepeat:'no-repeat',
-                padding: "40px"
-            }}>
             </div>
         </>
     )
