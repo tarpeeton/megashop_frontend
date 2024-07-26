b6d5dd31221e6f40016a99bb4d613af5afd79a3a
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-07-26 15:40:31 +0500

    Update LastContainer.jsx

--- src/ui/cotegories/LastContainer.jsx
+++ src/ui/cotegories/LastContainer.jsx
@@ -6,7 +6,7 @@ const LastContainer = () => {
     return (
         <div className={styles.LastContainer}>
             <div className={styles.BigContainer}>
-                <img src="https://www.goldenpages.uz/timur/1400909-16/avtozapchasti_rubrics.jpg" alt="image"/>
+                <img src="https://www.goldenpages.uz/timur/1400909-16/avtozapchasti-dlya-legkovyh-avtomobiley-uzbekistan_rubrics.jpg" alt="image"/>
                 <Link to="/product_list?categories=AvtomobilQismlari" className={styles.info}>
                     <p>Avtomobillar mahsuloti </p>
                     <h4>Avtomobillar uchun ehtiyot qismlar</h4>
@@ -22,7 +22,7 @@ const LastContainer = () => {
                         </div>
                     </Link>
                     <Link to="/product_list?categories=Elektronika" className={styles.GridChilder}>
-                        <img alt="elektronika" src="https://avatars.mds.yandex.net/get-altay/10637174/2a0000018a3712ce173f87b643e8265e8778/L_height"/>
+                        <img alt="elektronika" src="https://www.apple.com/v/macbook-air/r/images/overview/hero/hero_static__c9sislzzicq6_large.jpg"/>
                         <div className={styles.replaceToPage}>
                             <p>Elektronika</p>
                             <h4>Elektron mahsulotlar</h4>
@@ -32,7 +32,7 @@ const LastContainer = () => {
 
                 <div className={styles.GridChilderFull}>
                     <Link to="/product_list?categories=Maishiy Texnika" className={styles.GridChilder}>
-                        <img alt="Rasm" src="https://olmashop.uz/static/new/Household_appliances.png"/>
+                        <img alt="Rasm" src="&quot;https://frankfurt.apollo.olxcdn.com/v1/files/yresezz6tqdy2-UZ/image&quot;"/>
                         <div className={styles.InfoClick}>
                             <p>Maishiy texnika</p>
                             <h4>Kafolati Bilan !</h4>
